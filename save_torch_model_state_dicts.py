febfdf2042916736df5798f1c4260bae09af43f9
rkansal47 <rkansal@cern.ch>
2021-08-09 22:54:15 +0000

    bug fixes

--- save_torch_model_state_dicts.py
+++ save_torch_model_state_dicts.py
@@ -6,10 +6,13 @@ save_directory = '/graphganvol/mnist_graph_gan/jets/models/pcgan/'
 trainings = os.listdir(results_directory)
 
 for training in trainings:
+    print(training)
     full_path = results_directory + training + '/'
     models = os.listdir(full_path)
     filtered = [int(model[:-4].split('_')[-1]) for model in models if model[-4:] == '.pth']
-    final_model = filtered.sort()[-1]
+    filtered.sort()
+    final_model = filtered[-1]
+    print(final_model)
     G_inv = torch.load(f"{full_path}/G_inv_network_{final_model}.pth")
     G_pc = torch.load(f"{full_path}/G_network_{final_model}.pth")
 
@@ -18,5 +21,5 @@ for training in trainings:
 
     key = training[-1]
     if key == '1': key = 'g'
-    torch.save(G_inv.state_dict(), f"{full_path}/pcgan_G_inv_{key}.pt")
-    torch.save(G_pc.state_dict(), f"{full_path}/pcgan_G_pc_final_{key}.pt")
+    torch.save(G_inv.state_dict(), f"{save_directory}/pcgan_G_inv_{key}.pt")
+    torch.save(G_pc.state_dict(), f"{save_directory}/pcgan_G_pc_{key}.pt")
